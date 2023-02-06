30eb1269e34122f383762123b5ca9762d31746b0
Igor <fullstack@naladka.su>
2023-02-07 02:38:48 +0300

    edit task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write($"Введите 5-ти значное число: ");
+int namber = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [5]; 
+int index = 4;
+
+while (index > -1)
+{
+	array[index] = namber % 10;
+	namber = namber / 10;
+	index--;
+}
+
+if (array[0] == array[4] && array[1] == array[3])
+{
+	System.Console.WriteLine("Это число является палиндромом");
+}
+else 
+{
+	System.Console.WriteLine("Это число не является палиндромом");
+}
