e64bb66c39ddfbecebe84b16aaf21bc7146b30a1
Igor <fullstack@naladka.su>
2023-02-08 12:18:14 +0300

    edit task3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write($"Введите число N: ");
+int namberN = Convert.ToInt32(Console.ReadLine());
+
+double cube = 1;
+int count = 1;
+
+while (count < namberN + 1)
+{
+	cube = Math.Pow(count, 3);
+	System.Console.WriteLine(cube);
+	count++;
+}
